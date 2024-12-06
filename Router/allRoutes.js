8a4907d74b138c4d16151d8d63e0ea1c7c32ae79
Oceane-Lacote <oceane.lacote@outlook.fr>
2024-12-06 16:45:11 +0100

    connexion/inscription/profil

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,6 +1,10 @@
 import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/profil", "Profil", "/pages/profil.html"),
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
